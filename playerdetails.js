251238ff77028684e38406816c93f0445d41ccb6
sin-1337 <sinthesinner@proton.me>
2025-01-06 22:29:13 -0500

    Update playerdetails.js
    updated the code to be more efficient and count admins properly

--- playerdetails.js
+++ playerdetails.js
@@ -14,31 +14,9 @@
 // @run-at document-end
 // ==/UserScript==
 
-//Formats the player data for output to chat
-function playerToChar(PlayersInRoom, MemberNumber) {
-    const PLAYER = ChatRoomCharacter.find(C => C.MemberNumber == MemberNumber);
-    
-    //bail out and return placeholder if player is not available.
-    if (!PLAYER) {
-        return "- <span style='color:#FF0000'>[Unknown Person]</span>";
-    }
-
-    //Is the player an admin?
-    let PlayerIsAdmin = "";
-    if (ChatRoomData.Admin.includes(PLAYER.MemberNumber)) {
-        PlayerIsAdmin = "🛡️";
-    }
-
-    //return html output containing information about the player with clickable link
-    return `- <span style="color:${PLAYER.LabelColor || '#FFFFFF'}; cursor:pointer; 
-            text-shadow: 0px 0px 3px #000000; white-space: nowrap;" 
-            onclick="showPlayerImage(${PLAYER.MemberNumber})" 
-            onmouseover="this.style.textDecoration='underline';" 
-            onmouseout="this.style.textDecoration='none';">${CharacterNickname(PLAYER)}[${PLAYER.MemberNumber}]${PlayerIsAdmin}</span>`;
-}
-
-//Opens the player profile
-function showPlayerImage(MemberNumber) {
+// Opens the player profile
+// This functions is setup up to be exposed to the global DOM
+window.showPlayerImage = function (MemberNumber) {
     // Check if the person is still in the room
     const PLAYER = ChatRoomCharacter.find(C => C.MemberNumber == MemberNumber);
     if (PLAYER) {
@@ -47,25 +25,53 @@ function showPlayerImage(MemberNumber) {
     } else {
         ChatRoomSendLocal("This person is no longer in the room.");
     }
-}
+};
+
 
 CommandCombine([{
     Tag: 'players',
     Description: "Show the player count, helpful in maps.",
     Action: args => {
-        //output total number of players/admins
-        //TODO: make only this print if the "count" switch is specified
-        ChatRoomSendLocal("There are " + ChatRoomData.Admin.length + " admins for this room.")
-        ChatRoomSendLocal("There are " + ChatRoomCharacter.length + "/" + ChatRoomData.Limit + " total players in the room." );
-        
+
+        let output_html = "";
+        let player;
+        let admin_count = 0;
+        let player_is_admin ="";
         //get a list of players
-        let PlayersInRoom = [];
         for (let person in ChatRoomData.Character) {
-            PlayersInRoom.push(ChatRoomData.Character[person].MemberNumber);
+          // Reset player_is_admin
+          player_is_admin = "";
+
+          // find membernumber for current player in list
+          MemberNumber = ChatRoomData.Character[person].MemberNumber;
+
+          // Find player
+          player = ChatRoomCharacter.find(C => C.MemberNumber == MemberNumber);
+
+          //bail out and return placeholder if player is not available.
+          if (!player) {
+                output_html += "- <span style='color:#FF0000'>[Unknown Person]</span>\n";
+          }
+
+          // check if the player is an admin and update the count, also flad the player as admin in the output list.
+          let PlayerIsAdmin = "";
+          if (ChatRoomData.Admin.includes(player.MemberNumber)) {
+              admin_count++;
+              player_is_admin = "🛡️";
+          }
+
+          output_html += `- <span style="color:${player.LabelColor || '#FFFFFF'}; cursor:pointer;
+          text-shadow: 0px 0px 3px #000000; white-space: nowrap;"
+          onclick="showPlayerImage(${player.MemberNumber})"
+          onmouseover="this.style.textDecoration='underline';"
+          onmouseout="this.style.textDecoration='none';">${CharacterNickname(player)}[${player.MemberNumber}]${player_is_admin}</span>\n`;
         }
 
-        // Print the final list of player output in chat
-        const PLAYER_HTML = PlayersInRoom.map(MemberNumber => playerToChar(PlayersInRoom, MemberNumber)).join("\n");
-        ChatRoomSendLocal(`${PLAYER_HTML}`);
+
+        //output total number of players/admins
+        //TODO: make only this print if the "count" switch is specified
+        ChatRoomSendLocal("There are " + admin_count + "/" + ChatRoomData.Admin.length + " admins in the room.")
+        ChatRoomSendLocal("There are " + ChatRoomCharacter.length + "/" + ChatRoomData.Limit + " total players in the room." );
+        ChatRoomSendLocal(`${output_html}`);
     }
 }]);
