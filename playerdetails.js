804a9f0be9ca20a73f6584c871a0f6855f47a7da
sin-1337 <sinthesinner@proton.me>
2025-01-15 01:35:54 -0500

    Update playerdetails.js
    updated the output so that mousing over your own name will no longer change the cursor.

--- playerdetails.js
+++ playerdetails.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name playerdetails
 // @namespace https://www.bondageprojects.com/
-// @version 3.12
+// @version 3.13
 // @description Adds /players, shows info about players in the room, also adds whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
@@ -144,7 +144,7 @@ function formatoutput(player, badge, player_icons, isMe) {
 
   if (isMe) {
   // if the player is me, don't let me whisper myself
-    output += `<td style="padding-left: 5px; padding-right-5px; padding-bottom: 1px; padding-top: 0;"><span style="color:${player.LabelColor || '#FFFFFF'}; cursor:pointer;
+    output += `<td style="padding-left: 5px; padding-right-5px; padding-bottom: 1px; padding-top: 0;"><span style="color:${player.LabelColor || '#FFFFFF'};
                 font-family: Arial, sans-serif;
                 text-shadow: 0px 0px 1px #000000; white-space: nowrap;">
                   ${CharacterNickname(player)}[${player.MemberNumber}]
