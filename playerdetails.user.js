c2a05b1eca19b0496953063beed0b73254704919
sin-1337 <sinthesinner@proton.me>
2025-02-16 00:53:10 -0500

    Update playerdetails.user.js
    update version number

--- playerdetails.user.js
+++ playerdetails.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name playerdetails
 // @namespace https://www.bondageprojects.com/
-// @version 3.15
+// @version 3.16
 // @description Adds /players, shows info about players in the room, also adds whisper+
 // @author Sin
 // @match https://bondageprojects.elementfx.com/*
